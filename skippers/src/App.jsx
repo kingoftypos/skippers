49604ac9dba41131dbf4f3843e00283474608fde
Shreyansh mishra <mshreyansh121@gmail.com>
2024-11-12 02:14:06 +0530

    added shadcn-ui

--- skippers/src/App.jsx
+++ skippers/src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home"; 
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
